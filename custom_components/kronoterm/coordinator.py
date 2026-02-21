cc2d59cb343b31de41ed3875d638fad323ee6ce7
frelih <frelih@Debian-Asus.localdomain>
2026-02-21 23:32:06 +0100

    Fetch cloud consumption data for daily energy sensors

--- custom_components/kronoterm/coordinator.py
+++ custom_components/kronoterm/coordinator.py
@@ -269,6 +269,11 @@ class KronotermMainCoordinator(KronotermBaseCoordinator):
                     self._request_with_retries("GET", self.api_queries_get["loop4"]),
                     self._request_with_retries("GET", self.api_queries_get["main_settings"]),
                     self._request_with_retries("GET", self.api_queries_get["system_data"]),
+                    self._request_with_retries(
+                        "POST",
+                        self.api_queries_get["consumption"],
+                        CONSUMPTION_FORM_DATA_STATIC,
+                    ),
                 )
                 data["loop1"] = loop_results[0]
                 data["loop2"] = loop_results[1]
@@ -278,6 +283,7 @@ class KronotermMainCoordinator(KronotermBaseCoordinator):
                 data["loop4"] = loop_results[5]
                 data["main_settings"] = loop_results[6]
                 data["system_data"] = loop_results[7]
+                data["consumption"] = loop_results[8]
             except Exception as e:
                 _LOGGER.error("Error fetching loops: %s", e)
             
