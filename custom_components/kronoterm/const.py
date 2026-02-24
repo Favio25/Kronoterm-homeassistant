f1cb581554197a5708f2be3155626c9eab95bd18
frelih <frelih@Debian-Asus.localdomain>
2026-02-24 14:21:54 +0100

    Restore legacy cloud consumption request

--- custom_components/kronoterm/const.py
+++ custom_components/kronoterm/const.py
@@ -119,8 +119,9 @@ MAIN_MODE_OPTIONS = {
     2: "comfort",
 }
 
-# Base form data for consumption fetching (date params added dynamically)
-CONSUMPTION_FORM_BASE = [
+# Static form data for consumption fetching
+CONSUMPTION_FORM_DATA_STATIC = [
+    ("d2", "0"),
     ("type", "day"),
     # aValues:
     ("aValues[]", "17"),
